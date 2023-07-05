3c40239754ad5e93ad284d791b56aad134324c95
José Campelo <josecampelojr@gmail.com>
2023-07-05 12:54:34 -0300

    Finalizando o projeto, adicionando toda a lógica para criar uma coroutine e suas operações básicas.

--- app/src/main/java/com/campelo/coroutines/MainActivity.kt
+++ app/src/main/java/com/campelo/coroutines/MainActivity.kt
@@ -2,10 +2,58 @@ package com.campelo.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.campelo.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnExecutar.setOnClickListener {
+            job = CoroutineScope(Dispatchers.IO).launch {
+                withTimeout(7000L){
+                    executar()
+                }
+            }
+        }
+
+        binding.btnEncerrar.setOnClickListener {
+            job?.cancel()
+            botaoTrue()
+        }
+    }
+
+    private suspend fun executar(){
+        repeat(10){i ->
+            withContext(Dispatchers.Main){
+                binding.btnExecutar.text = "Executando T:${Thread.currentThread().name} i:$i"
+                binding.btnExecutar.isEnabled = false
+            }
+
+            delay(1000)
+        }
+
+        withContext(Dispatchers.Main){
+            botaoTrue()
+        }
+    }
+
+    private fun botaoTrue(){
+        binding.btnExecutar.text = "Executar"
+        binding.btnExecutar.isEnabled = true
     }
 }
